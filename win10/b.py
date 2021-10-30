40b964093bb92b14669968aa91817e6344db887a
genkinaoko <surtaka944@gmail.com>
2021-10-30 12:01:57 +0900

    from pro

---
+++ win10/b.py
@@ -0,0 +1,23 @@
+length = int(input())
+kai = ""
+
+px = 0
+py = 0
+
+for i in range(length):
+    t, x, y = map(int, input().split())
+    time = t
+    for j in range(t*4):
+
+
+        if px == x and py == y:
+            
+        if j % time == 2:
+            px = 0
+            py = 0
+
+
+
+
+
+
